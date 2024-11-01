a6895a878f0f5dd16f1fe4fb145492ea320cf6e5
demo122500 <152680493+demo122500@users.noreply.github.com>
2024-11-01 17:06:43 +0800

    Update server.js

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,2 +1,2 @@
-export const server = "http://localhost:8000/api/v2" || "https://g-market-snowy.vercel.app/api/v2";
-export const backend_url = "http://localhost:8000 " || "https://g-market-snowy.vercel.app";
+export const server = "http://localhost:8000/api/v2" && "https://g-market-snowy.vercel.app/api/v2";
+export const backend_url = "http://localhost:8000 " && "https://g-market-snowy.vercel.app";
