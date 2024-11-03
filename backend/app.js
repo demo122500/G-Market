1a7af1666f0b20148e646a3f009b1adf1ee06e73
Demo Acct <demo122500@gmail.com>
2024-11-03 20:10:13 +0800

    ---

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,7 @@ const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
-const allowedOrigins = ["https://gmarket-delta.vercel.app"];
+const allowedOrigins = ["https://gmarket-delta.vercel.app", "http://localhost:3000"];
 
 app.use(
   cors({
