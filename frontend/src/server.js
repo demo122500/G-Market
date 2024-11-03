ae03e030b4d221ed907e294612efc251d2950679
Demo Acct <demo122500@gmail.com>
2024-11-03 20:09:26 +0800

    ---

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,2 +1,5 @@
 export const server = "https:g-market-snowy.vercel.app/api/v2";
 export const backend_url = "https:g-market-snowy.vercel.app";
+
+// https:g-market-snowy.vercel.app
+// http://localhost:8000
