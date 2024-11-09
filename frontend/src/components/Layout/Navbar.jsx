baa04076c4c34a1f6e85e2b29bbf2bae9d722e34
Demo Acct <demo122500@gmail.com>
2024-11-09 20:22:06 +0800

    16th-commit

--- frontend/src/components/Layout/Navbar.jsx
+++ frontend/src/components/Layout/Navbar.jsx
@@ -1,25 +1,23 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { Link } from "react-router-dom";
 import { navItems } from "../../static/data";
 import styles from "../../styles/styles";
 
 const Navbar = ({ active }) => {
-  return (
-    <div className={`block 800px:${styles.noramlFlex}`}>
-      {navItems.map((i, index) => (
-        <div key={index} className="flex">
-          <Link
-            to={i.url}
-            className={`pb-[30px] 800px:pb-0 font-[500] px-6 cursor-pointer ${
-              active === index + 1 ? "text-[#000000]" : "text-black 800px:text-[#fff]"
-            }`}
-          >
-            {i.title}
-          </Link>
-        </div>
-      ))}
-    </div>
-  );
+  const renderedNavItems = useMemo(() => {
+    return navItems.map((item, index) => (
+      <div key={index} className="flex">
+        <Link
+          to={item.url}
+          className={`${active === index + 1 ? "text-[#000000]" : "text-black 800px:text-[#fff]"} pb-[30px] 800px:pb-0 font-[500] px-6 cursor-pointer`}
+        >
+          {item.title}
+        </Link>
+      </div>
+    ));
+  }, [active]);
+
+  return <div className={`block 800px:${styles.noramlFlex}`}>{renderedNavItems}</div>;
 };
 
 export default Navbar;
