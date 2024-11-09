53e2df0bc730b271bcc6ea9ada57f38501d0e4eb
Demo Acct <demo122500@gmail.com>
2024-11-09 20:16:21 +0800

    15th-commit

--- frontend/src/components/Layout/Navbar.jsx
+++ frontend/src/components/Layout/Navbar.jsx
@@ -6,21 +6,18 @@ import styles from "../../styles/styles";
 const Navbar = ({ active }) => {
   return (
     <div className={`block 800px:${styles.noramlFlex}`}>
-      {navItems &&
-        navItems.map((i, index) => (
-          <div key={index} className="flex">
-            <Link
-              to={i.url}
-              className={`${
-                active === index + 1
-                  ? "text-[#000000]"
-                  : "text-black 800px:text-[#fff]"
-              } pb-[30px] 800px:pb-0 font-[500] px-6 cursor-pointer}`}
-            >
-              {i.title}
-            </Link>
-          </div>
-        ))}
+      {navItems.map((i, index) => (
+        <div key={index} className="flex">
+          <Link
+            to={i.url}
+            className={`pb-[30px] 800px:pb-0 font-[500] px-6 cursor-pointer ${
+              active === index + 1 ? "text-[#000000]" : "text-black 800px:text-[#fff]"
+            }`}
+          >
+            {i.title}
+          </Link>
+        </div>
+      ))}
     </div>
   );
 };
